38f3dfec506d68bb9ccd959c117fc9f9492fd25b
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-07-30 11:22:09 +0200

    installed dependencies for icons

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./Cards.module.css";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 
 const Cards = () => {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <p>Moiiin Luis</p>
-        <button className={styles.playButton}>Play</button>
+        <button className={styles.playButton}>
+          <PlayArrowIcon style={{fontSize: 40}}/>
+        </button>
       </div>
     </div>
   );
