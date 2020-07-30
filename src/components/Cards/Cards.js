ca8f1a1cefd81853886de438c9a9fad448799d78
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-07-30 11:59:44 +0200

    example card plays sound now

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,13 +2,18 @@ import React from "react";
 import styles from "./Cards.module.css";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 
+import useSound from "use-sound";
+import exampleSound from "../../sounds/Moin_Luis.mp3";
+
 const Cards = () => {
+  const [play] = useSound(exampleSound);
+
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <p>Moiiin Luis</p>
-        <button className={styles.playButton}>
-          <PlayArrowIcon style={{fontSize: 40}}/>
+        <button className={styles.playButton} onClick={play}>
+          <PlayArrowIcon style={{ fontSize: 40 }} />
         </button>
       </div>
     </div>
