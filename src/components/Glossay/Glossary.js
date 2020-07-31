264bff843424758cd61ed9f1693715c50dcd861f
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-07-31 11:33:41 +0200

    added buttons to go back to homepage to sounds and glossary

--- src/components/Glossay/Glossary.js
+++ src/components/Glossay/Glossary.js
@@ -1,11 +1,25 @@
 import React from "react";
+import styles from "./Glossary.module.css";
+
+import KeyboardBackspaceRoundedIcon from "@material-ui/icons/KeyboardBackspaceRounded";
+
+import { Link } from "react-router-dom";
 
 const Glossary = () => {
-    return (
-        <div>
-            <h1>Here comes the Glossary</h1>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div>
+        <Link to="/">
+          <button className={styles.backHome}>
+            <KeyboardBackspaceRoundedIcon />
+          </button>
+        </Link>
+      </div>
+      <div className={styles.glossary}>
+        <h2 className={styles.heading}>Glossar</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Glossary;
