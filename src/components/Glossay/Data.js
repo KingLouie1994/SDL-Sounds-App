29263f163f742999a1d96998b97d9da950107062
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-08-04 09:17:26 +0200

    restructured glossary data

--- src/components/Glossay/Data.js
+++ src/components/Glossay/Data.js
@@ -1,12 +1,12 @@
 const Data = [
   { id: 0, shortcut: "At", explanation: "Abtransport" },
-  { id: 1, shortcut: "wS", explanation: "wie Schwein" },
-  { id: 2, shortcut: "bwS", explanation: "breit wie Schwein" },
-  { id: 3, shortcut: "SDL", explanation: "Set Dirim Lah" },
-  { id: 4, shortcut: "iA", explanation: "im Arsch" },
+  { id: 1, shortcut: "bwS", explanation: "breit wie Schwein" },
+  { id: 2, shortcut: "iA", explanation: "im Arsch" },
+  { id: 3, shortcut: "iAwS", explanation: "im Arsch wie Schwein" },
+  { id: 4, shortcut: "mA", explanation: "mit Alkohol" },
   { id: 5, shortcut: "oA", explanation: "ohne Alkohol" },
-  { id: 6, shortcut: "mA", explanation: "mit Alkohol" },
-  { id: 7, shortcut: "iAwS", explanation: "im Arsch wie Schwein" },
+  { id: 6, shortcut: "SDL", explanation: "Set Dirim Lah" },
+  { id: 7, shortcut: "wS", explanation: "wie Schwein" },
 ];
 
 export default Data;
