c00cde814302bc0e23281bcd25e09bb4bdd3185d
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-08-13 14:48:50 +0200

    filters commented out | search is styled

--- src/components/Glossay/Glossary.js
+++ src/components/Glossay/Glossary.js
@@ -2,45 +2,66 @@ import React, { useState } from "react";
 
 import styles from "./Glossary.module.css";
 import Data from "./Data";
-import Filter from "../Filter/Filter";
+// import Filter from "../Filter/Filter";
 
 import { Link } from "react-router-dom";
 
 import KeyboardBackspaceRoundedIcon from "@material-ui/icons/KeyboardBackspaceRounded";
 import SearchIcon from "@material-ui/icons/Search";
 
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+
+const CssTextField = withStyles({
+  root: {
+    "& label": {
+      color: "white",
+    },
+    "& label.Mui-focused": {
+      color: "white",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "white",
+    },
+    "& .MuiInput-underline:before": {
+      borderBottomColor: "white",
+    },
+  },
+})(TextField);
+
 const Glossary = () => {
-  const [sort, setSort] = useState("");
+  // const [sort, setSort] = useState("");
   const [search, setSearch] = useState("");
 
-  const fromAToZ = (a, b) => {
-    if (a.id < b.id) {
-      return -1;
-    } else if (a.id > b.id) {
-      return 1;
-    } else {
-      return 0;
-    }
-  };
+  // const fromAToZ = (a, b) => {
+  //   if (a.id < b.id) {
+  //     return -1;
+  //   } else if (a.id > b.id) {
+  //     return 1;
+  //   } else {
+  //     return 0;
+  //   }
+  // };
 
-  const fromZToA = (a, b) => {
-    if (a.id > b.id) {
-      return -1;
-    } else if (a.id < b.id) {
-      return 1;
-    } else {
-      return 0;
-    }
-  };
+  // const fromZToA = (a, b) => {
+  //   if (a.id > b.id) {
+  //     return -1;
+  //   } else if (a.id < b.id) {
+  //     return 1;
+  //   } else {
+  //     return 0;
+  //   }
+  // };
 
-  const handleSortChange = (e) => {
-    setSort(e.target.value);
-    if (e.target.value === "zToA") {
-      Data.sort(fromZToA);
-    } else {
-      Data.sort(fromAToZ);
-    }
-  };
+  // const handleSortChange = (e) => {
+  //   setSort(e.target.value);
+  //   if (e.target.value === "zToA") {
+  //     Data.sort(fromZToA);
+  //   } else {
+  //     Data.sort(fromAToZ);
+  //   }
+  // };
 
   const editSearchTerm = (e) => {
     setSearch(e.target.value);
@@ -58,19 +79,24 @@ const Glossary = () => {
       <div className={styles.glossary}>
         <h2 className={styles.heading}>Glossar</h2>
         <div className={styles.filterAndSearch}>
-          <div className={styles.filter}>
+          {/* <div className={styles.filter}>
             <Filter handleSortChange={handleSortChange} />
-          </div>
-          <div className={styles.search}>
-            <SearchIcon />:
-            <input
-              className={styles.searchInput}
-              type="text"
-              value={search}
-              onChange={editSearchTerm}
-              placeholder="Search..."
-            />
-          </div>
+          </div> */}
+          <CssTextField
+            type="text"
+            value={search}
+            onChange={editSearchTerm}
+            className={styles.searchInput}
+            id="custom-css-standard-input"
+            label="Search..."
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon style={{ color: "white" }} />
+                </InputAdornment>
+              ),
+            }}
+          />
         </div>
         {Data.map((shortcut) => {
           if (shortcut.shortcut.toLowerCase().includes(search.toLowerCase())) {
